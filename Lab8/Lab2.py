bb55ae95e1887a4300c816d69c15e63ccb595ebd
Byte Supanat <byte_supanat@outlook.com>
2014-03-13 20:50:28 +0700

    Add 2 problem file

--- Lab8/Lab2.py
+++ Lab8/Lab2.py
@@ -1,2 +1,33 @@
-import PySide.QtGui
-import PySide.QtCore
+from PySide import QtGui, QtCore
+
+class Window(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.view = View(self)
+        self.button = QtGui.QPushButton('Clear View', self)
+        self.button.clicked.connect(self.handleClearView)
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.view)
+        layout.addWidget(self.button)
+
+    def handleClearView(self):
+        self.view.scene().clear()
+
+class View(QtGui.QGraphicsView):
+    def __init__(self, parent):
+        self.scene = QtGui.QGraphicsScene(self)
+        QtGui.QGraphicsView.__init__(self, parent)
+        self.setScene(self.scene)
+        self.setSceneRect(QtCore.QRectF(self.viewport().rect()))
+
+    def mousePressEvent(self, event):
+        self.scene.
+
+if __name__ == '__main__':
+
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    window.resize(640, 480)
+    window.show()
+    sys.exit(app.exec_())
