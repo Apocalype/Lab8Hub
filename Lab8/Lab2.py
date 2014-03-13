eb9e2f26b3539f949792a4dcd51eb2b01703cc68
Byte Supanat <byte_supanat@outlook.com>
2014-03-13 21:28:45 +0700

    Problem 2_Final

--- Lab8/Lab2.py
+++ Lab8/Lab2.py
@@ -4,24 +4,46 @@ class Window(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.view = View(self)
-        self.button = QtGui.QPushButton('Clear View', self)
-        self.button.clicked.connect(self.handleClearView)
+        self.button = QtGui.QPushButton("Clear")
+        self.button.clicked.connect(self.Clear)
         layout = QtGui.QVBoxLayout(self)
         layout.addWidget(self.view)
         layout.addWidget(self.button)
-
-    def handleClearView(self):
-        self.view.scene().clear()
+    
+    def Clear(self):
+        self.view.Clear()
 
 class View(QtGui.QGraphicsView):
     def __init__(self, parent):
-        self.scene = QtGui.QGraphicsScene(self)
+        self.pressing = False
+        self.scene = QtGui.QGraphicsScene()
+        self.x = 0
+        self.y = 0
+
         QtGui.QGraphicsView.__init__(self, parent)
         self.setScene(self.scene)
         self.setSceneRect(QtCore.QRectF(self.viewport().rect()))
+    def Clear(self):
+        self.scene = QtGui.QGraphicsScene()
+        self.setScene(self.scene)
+
+    def mouseMoveEvent(self, event):
+        if(self.pressing):
+            rad = 1
+            pos = self.mapToScene(event.pos())
+            self.scene.addLine(self.x,self.y,pos.x()-rad,pos.y()-1, QtGui.QPen())
+        self.x = pos.x()-rad
+        self.y = pos.y()-rad
 
     def mousePressEvent(self, event):
-        self.scene.
+        rad = 1
+        self.pressing = True
+        pos = self.mapToScene(event.pos())
+        self.x = pos.x()-rad
+        self.y = pos.y()-rad
+
+    def mouseReleaseEvent(self, event):
+        self.pressing = False
 
 if __name__ == '__main__':
 
